5982dd3d9be94379cea6828918da409efb7efe82
StevenJBurns <StevenJBurns@users.noreply.github.com>
2023-06-09 10:00:50 -0600

    chore: clean up ObservableObject.cs

--- WPF/ViewModel/ObservableObject.cs
+++ WPF/ViewModel/ObservableObject.cs
@@ -2,15 +2,15 @@
 
 namespace SJB.Sol.ViewModel
 {
-    public class ObservableObject : INotifyPropertyChanged
+  public class ObservableObject : INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler PropertyChanged;
+    
+    protected void RaisePropertyChangedEvent(string propertyName)
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected void RaisePropertyChangedEvent(string propertyName)
-        {
-            var handler = PropertyChanged;
-            if (handler != null)
-                handler(this, new PropertyChangedEventArgs(propertyName));
-        }
+      var handler = PropertyChanged;
+      if (handler != null)
+        handler(this, new PropertyChangedEventArgs(propertyName));
     }
+  }
 }
